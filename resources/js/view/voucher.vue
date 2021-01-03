3db7a7db74fa2b07d56cb78de12ce9df40540072
agust-D2 <nathan.s.untels@gmail.com>
2021-01-02 20:43:52 -0600

    materia

--- resources/js/view/voucher.vue
+++ resources/js/view/voucher.vue
@@ -1,6 +1,6 @@
 <template>
     <div clas="voucher" >
-        Este es un voucher fork Erick untels of Nathan
+        Este es un voucher fork Erick untels of Nathan UNTELS
         Este es un voucher fork Nathan 
     </div>
 </template>
