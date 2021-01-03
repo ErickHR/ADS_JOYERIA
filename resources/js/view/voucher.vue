b2594a4e14c7e02e3e1c0e5d0521c9ea77b308a5
agust-D2 <nathan.s.untels@gmail.com>
2021-01-02 20:35:59 -0600

    untels ads

--- resources/js/view/voucher.vue
+++ resources/js/view/voucher.vue
@@ -1,7 +1,7 @@
 <template>
     <div clas="voucher" >
         Este es un voucher fork Erick untels of Nathan
-        Este es un voucher fork Nathan
+        Este es un voucher fork Nathan 
     </div>
 </template>
 
